fc9a98e4a352e414f0a432bad93dba680c59a1d0
shoblinsky <timosh0307@gmail.com>
2025-09-14 11:16:20 +0500

    feat: 20 tests passing

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -388,8 +388,9 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const result = value.replace(/Hello, /g, '').replace(/!/g, '');
+  return result;
 }
 
 /**
