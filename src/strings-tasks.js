9fa880f4fc894c4d7716c1dc4b8344184576cd2a
shoblinsky <timosh0307@gmail.com>
2025-09-14 14:48:38 +0500

    feat: 24 tests passing

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -342,8 +342,13 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const strArray = str.split(' ');
+  const result = [];
+  for (let i = 0; i < strArray.length; i += 1) {
+    result.push(strArray[i].split('').reverse().join(''));
+  }
+  return result.join(' ');
 }
 
 /**
