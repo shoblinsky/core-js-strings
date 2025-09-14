63b0c4366bb8d4a065da14ee69312f126a436a32
shoblinsky <timosh0307@gmail.com>
2025-09-14 16:00:09 +0500

    feat: 31 tests passing

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -185,8 +185,13 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (str === undefined || str === null) return 0;
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
